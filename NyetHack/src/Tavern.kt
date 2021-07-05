ec30ef802181be21c1fd7566544c206ae81b6af4
jinhaEom <84216838+jinhaEom@users.noreply.github.com>
2021-07-05 12:17:16 +0900

    Add files via upload

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,12 +1,35 @@
+const val TAVERN_NAME = "Taernyl's Folly"
 fun main() {
-    var beverage = readLine()?.let{
-        if(it.isNotBlank()){
-            it.capitalize()
-        }else{
-            "맥주"
+    //placeOrder("shandy, Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
+
+}
+private fun toDragonSpeak(pharse:String)=
+    pharse.replace(Regex("[aeiou]")){
+        //Regex함수는 찾아서 변경할 문자가 정의된 검색패턴인 "[aeiou]"를 인자로 받는다.
+        when(it.value){
+            "a"->"4"
+            "e"->"3"
+            "i"->"1"
+            "o"->"0"
+            "u"->"|_|"
+            else->it.value
         }
     }
-    //    beverage=null
+private fun placeOrder(menuData:String){
+    val indexOfApostrophe=TAVERN_NAME.indexOf('\'')
+        val tavernMaster=TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("마드리드같은 $tavernMaster 에게 주문한다.")
 
-    println(beverage)
+    val(type,name,price)=menuData.split(',')//
+    val message="마드리갈은 금화 $price 로 $name ($type)를 구입한다."
+    println(message)
+
+    val phrase = if(name=="Dragon's Breath"){
+        "마드리갈이 감탄한다: ${toDragonSpeak(("와 $name 진짜 좋구나"))}"
+
+    }else{
+        "마드리갈이 말한다: 감사합니다 $name."
+    }
+    println(phrase)
 }
